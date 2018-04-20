4317f8012079815c78ae5e5309a2b97fd216c6b4
Cristiano Chiaramelli <cristianochiaramelli@gmail.com>
2018-04-20 18:06:24 -0300

    Storing

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const User = require('./src/User');
 const Email = require('./src/Email');
 const UserEmail = require('./src/UserEmail');
 
+const storeUser = async(user) => {
+	return User.query().insert({name: user.name, email: user.email})
+}
+const storeEmail = async(email) => {
+	return Email.query().insert({subject: email.subject, text: email.text})
+}
+
 module.exports = (req, res) => {	
 	res.end('Welcome to Micro')
 }
